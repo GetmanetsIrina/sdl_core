3ca3b3fc43118b0385883ca21258490c7d7a4ad8
LitvinenkoIra <ilytvynenko@luxoft.com>
2019-06-04 16:57:55 +0300

    Define Grid, add functions for getting moduleServiceArea and allowMultipleAccess values
    Add Seat Location capabilities checking and function for getting driver's location
    Check if the user location area and the service area has something in common, or the user location is driver,
    otherwise - reject the module reallocation request
    
    Fix UTs after adding the location check

--- src/components/application_manager/include/application_manager/hmi_capabilities_impl.h
+++ src/components/application_manager/include/application_manager/hmi_capabilities_impl.h
@@ -501,6 +501,11 @@ class HMICapabilitiesImpl : public HMICapabilities {
 
   const smart_objects::SmartObject* rc_capability() const OVERRIDE;
 
+  void set_seat_capability(
+      const smart_objects::SmartObject& seat_capability) OVERRIDE;
+
+  const smart_objects::SmartObject* seat_capability() const OVERRIDE;
+
   void Init(resumption::LastState* last_state) OVERRIDE;
 
   /*
@@ -585,6 +590,7 @@ class HMICapabilitiesImpl : public HMICapabilities {
   smart_objects::SmartObject* phone_capability_;
   smart_objects::SmartObject* video_streaming_capability_;
   smart_objects::SmartObject* rc_capability_;
+  smart_objects::SmartObject* seat_capability_;
 
   ApplicationManager& app_mngr_;
   HMILanguageHandler hmi_language_handler_;
